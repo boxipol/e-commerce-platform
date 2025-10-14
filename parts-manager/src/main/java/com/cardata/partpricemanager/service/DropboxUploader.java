f7c51befafa96a3ba1daf0f3b7bb83997b021116
Petyo <petodo@abv.bg>
2025-10-14 12:38:40 +0300

    added sharing folder

--- parts-manager/src/main/java/com/cardata/partpricemanager/service/DropboxUploader.java
+++ parts-manager/src/main/java/com/cardata/partpricemanager/service/DropboxUploader.java
@@ -1,27 +1,33 @@
 package com.cardata.partpricemanager.service;
 
+import com.cardata.partpricemanager.brands.VehicleBrandTask;
+import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.FileMetadata;
 import com.dropbox.core.v2.files.WriteMode;
+import com.dropbox.core.v2.sharing.AccessLevel;
+import com.dropbox.core.v2.sharing.AddMember;
+import com.dropbox.core.v2.sharing.MemberSelector;
+import com.dropbox.core.v2.sharing.ShareFolderLaunch;
+import com.dropbox.core.v2.sharing.SharedFolderMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Path;
-
+import java.util.Collections;
 
 @Service
 public class DropboxUploader {
 
-	@Value("${dropbox.access.token}")
-	private String accessToken;
+	private final DbxClientV2 client;
 
 
-	public FileMetadata uploadFile(Path localFilePath, String dropboxPath) throws IOException {
+	public DropboxUploader(@Value("${dropbox.access.token}") String accessToken) {
 		DbxRequestConfig config = DbxRequestConfig.newBuilder("parts-manager").build();
-		DbxClientV2 client = new DbxClientV2(config, accessToken);
+		client = new DbxClientV2(config, accessToken);
+	}
 
 	public FileMetadata uploadFile(Path localFilePath, String dropboxPath) throws IOException {
 		try (var input = new FileInputStream(localFilePath.toString())) {
@@ -32,5 +38,19 @@ public class DropboxUploader {
 		} catch (Exception e) {
 			throw new IOException("Error uploading to Dropbox", e);
 		}
+	}
+
+	public SharedFolderMetadata shareFolderByMail(String mail) throws DbxException {
+		String dropBoxFolderPath = String.format("/Parts %s %s", VehicleBrandTask.CURRENT_DATE.getMonth(), VehicleBrandTask.CURRENT_DATE.getYear());
+		ShareFolderLaunch launch = client.sharing().shareFolder(dropBoxFolderPath);
+		String dropBoxFolderId = launch.getCompleteValue().getSharedFolderId();
+		AddMember member = new AddMember(MemberSelector.email(mail), AccessLevel.OWNER);
+
+		client.sharing()
+			.addFolderMember(dropBoxFolderId, Collections.singletonList(member));
+
+		ShareFolderLaunch metadata = client.sharing().shareFolder(dropBoxFolderPath);
+
+		return metadata.getCompleteValue();
 	}
 }
