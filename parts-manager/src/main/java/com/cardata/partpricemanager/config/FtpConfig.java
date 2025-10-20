62982b5aa85d3582b78228db9c78b4676bc26333
Petyo <petodo@abv.bg>
2025-10-20 13:11:46 +0300

    parts manager removed

--- parts-manager/src/main/java/com/cardata/partpricemanager/config/FtpConfig.java
+++
@@ -1,9 +0,0 @@
-package com.cardata.partpricemanager.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-
-@Configuration
-@EnableConfigurationProperties(FtpProperties.class)
-public class FtpConfig {}
