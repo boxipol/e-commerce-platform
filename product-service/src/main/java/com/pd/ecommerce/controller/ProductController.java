ffda45b54fae68b85a56e2acf356e5852d0b71f9
Petyo <petodo@abv.bg>
2026-05-09 21:07:14 +0300

    added gateway routing

--- product-service/src/main/java/com/pd/ecommerce/controller/ProductController.java
+++ product-service/src/main/java/com/pd/ecommerce/controller/ProductController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
