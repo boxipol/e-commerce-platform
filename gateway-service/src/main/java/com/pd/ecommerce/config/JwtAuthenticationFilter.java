add261a40832ec2ac60cae0ba706d072ff860f3a
Petyo <petodo@abv.bg>
2026-05-09 20:33:17 +0300

    setup basic security

---
+++ gateway-service/src/main/java/com/pd/ecommerce/config/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.pd.ecommerce.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+import java.util.List;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter implements WebFilter {
+
+	private final JwtService jwtService;
+
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+		String authHeader = exchange.getRequest()
+			.getHeaders()
+			.getFirst(HttpHeaders.AUTHORIZATION);
+
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			return chain.filter(exchange);
+		}
+
+		String token = authHeader.substring(7);
+
+		if (!jwtService.isTokenValid(token)) {
+			return chain.filter(exchange);
+		}
+
+		String username = jwtService.extractUsername(token);
+		var auth = new UsernamePasswordAuthenticationToken(username, null, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+		return chain.filter(exchange)
+			.contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth));
+	}
+}
--- gateway-service/src/main/java/com/pd/ecommerce/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/pd/ecommerce/config/SecurityConfig.java
@@ -2,22 +2,31 @@ package com.pd.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
-@EnableWebFluxSecurity
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
 	@Bean
-	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 		return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
-			.authorizeExchange(exchanges -> exchanges.anyExchange()
+			.httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+			.formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+			.authorizeExchange(ex -> ex.pathMatchers("/auth/**")
+				.permitAll()
+				.pathMatchers("/actuator/**")
+				.permitAll()
+				.anyExchange()
 				.authenticated())
-			.httpBasic(Customizer.withDefaults())
+//			.authorizeExchange(ex -> ex.pathMatchers("/admin/**")
+//				.hasRole("ADMIN")
+//				.pathMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+//				.anyExchange()
+//				.authenticated()
 			.build();
 	}
 }
