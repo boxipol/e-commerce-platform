f347e02a5f8167640371e37a6836881c97be8648
Petyo <petodo@abv.bg>
2026-05-12 22:03:39 +0300

    basic user service implementation

---
+++ user-service/src/main/java/com/pd/ecommerce/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.pd.ecommerce.repository;
+
+import com.pd.ecommerce.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+	Optional<User> findByEmail(String email);
+}
