6dc73b0e6e6540560cb1753a2fb99620604e37ab
Petyo <petodo@abv.bg>
2026-06-15 23:50:23 +0300

    project publicOrderId and mail

--- order-service/src/main/resources/postgres.sql
+++ order-service/src/main/resources/postgres.sql
@@ -2,22 +2,28 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE orders
 (
-    id           UUID PRIMARY KEY                  DEFAULT gen_random_uuid(),
-    user_id      UUID                     NOT NULL,
-    status       VARCHAR(30)              NOT NULL,
-    total_amount NUMERIC(12, 2)           NOT NULL,
-    created_at   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-    updated_at   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+    id              UUID PRIMARY KEY                  DEFAULT gen_random_uuid(),
+    public_order_id VARCHAR(40)              NOT NULL UNIQUE,
+    user_id         UUID                     NOT NULL,
+    user_mail       VARCHAR(255)             NOT NULL,
+    status          VARCHAR(30)              NOT NULL,
+    total_amount    NUMERIC(12, 2)           NOT NULL,
+    created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
 CREATE INDEX idx_orders_user_id
     ON orders (user_id);
+
+CREATE INDEX idx_orders_public_order_id
+    ON orders (public_order_id);
 
 CREATE TABLE order_items
 (
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     order_id   UUID           NOT NULL,
     product_id UUID           NOT NULL,
+    sku        VARCHAR(100)   NOT NULL,
     quantity   INT            NOT NULL,
     unit_price NUMERIC(12, 2) NOT NULL,
     subtotal   NUMERIC(12, 2) NOT NULL,
--- payment-service/src/main/resources/postgres.sql
+++ payment-service/src/main/resources/postgres.sql
@@ -1,11 +1,12 @@
-
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE payments
 (
     id                  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     order_id            UUID                        NOT NULL,
+    public_order_id     VARCHAR(40)                 NOT NULL,
     user_id             UUID                        NOT NULL,
+    user_mail           VARCHAR(255)                NOT NULL,
     amount              NUMERIC(19, 2)              NOT NULL,
     items               JSONB                       NOT NULL,
     currency            VARCHAR(10)                 NOT NULL,
@@ -19,6 +20,7 @@ CREATE TABLE payments
 );
 
 CREATE INDEX idx_payments_order_id ON payments (order_id);
+CREATE INDEX idx_payments_public_order_id ON payments (public_order_id);
 CREATE INDEX idx_payments_user_id ON payments (user_id);
 CREATE INDEX idx_payments_status ON payments (status);
 CREATE INDEX idx_payments_provider_payment_id ON payments (provider_payment_id);
